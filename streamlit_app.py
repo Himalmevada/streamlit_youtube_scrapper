74126355f933aad5304b43f81ccca6e7a1463a24
Himal Mevada <63632500+Himalmevada@users.noreply.github.com>
2024-03-07 10:19:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,23 +6,25 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import streamlit as st
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service as ChromeService
+# from webdriver_manager.chrome import ChromeDriverManager
 
 @st.cache_data()
 def scrape_youtube_data(url):
     # driver = webdriver.Chrome() # Offline
     # driver = webdriver.Chrome(ChromeDriverManager().install())  # Online
     # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+    # options = webdriver.ChromeOptions()
+    # options.add_argument("--headless")
+    # options.add_argument("--disable-gpu")
+    # options.add_argument("--no-sandbox")
+    # options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
-    
+    # driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(url)
     driver.maximize_window()
 
